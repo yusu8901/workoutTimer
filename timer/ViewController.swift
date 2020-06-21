f1e51c5de771cec7113d528ac33cd8c576f7ceb6
yusuke <y.s20168901@gmail.com>
2020-06-21 18:31:07 +0900

    タイマー基本機能完成

--- timer/ViewController.swift
+++ timer/ViewController.swift
@@ -9,12 +9,58 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var label: UILabel!
+    
+    var count: Float = 0.0
+    
+    var timer: Timer = Timer()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func startAndStop() {
+            
+            if !timer.isValid {
+                //タイマーが動作していなかったら動かす
+                timer = Timer.scheduledTimer(timeInterval: 0.01,
+                                             target: self,
+                                             selector: #selector(self.up),
+                                             userInfo: nil,
+                                             repeats: true)
+                
+                
+                
+            } else if timer.isValid {
+                //もしタイマーが動いていたら停止する
+                timer.invalidate()
+        }
+            
+        
+        }
+        
+        
+        
+        @IBAction func reset() {
+            
+            if timer.isValid {
+                timer.invalidate()
+            } else if !timer.isValid {
+                count = 0.00
+                label.text = String(format:"%.2f", count)
+            }
+            
+        }
+        
+       @objc func up() {
+              //countを0.01足す
+              count = count - 0.01
+              //ラベルに小数点以下２桁まで表示
+              label.text = String(format: "%.2f", count)
+          }
 
 
-}
-
+    }
